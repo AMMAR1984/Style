741089317cd4b76da857bf5c17fe9d1671db2faf
AMMAR1984 <54781984+AMMAR1984@users.noreply.github.com>
2020-05-06 01:41:05 +0100

    Add files via upload

---
+++ style.sh
@@ -0,0 +1,43 @@
+
+clear
+
+echo -e "\033[37;40;0m"
+
+echo -e "\033[1;37;40m  If you love this kind of press Line 1\n"
+echo -e "\033[37;40;0m"
+echo -e "\033[2;37;40m  If you love this kind of press Line 2\n"
+echo -e "\033[37;40;0m"
+echo -e "\033[3;37;40m  If you love this kind of press Line 3\n"
+echo -e "\033[37;40;0m"
+echo -e "\033[4;37;40m  If you love this kind of press Line 4\n"
+echo -e "\033[37;40;0m"
+echo -e "\033[5;37;40m  If you love this kind of press Line 5\n"
+echo -e "\033[37;40;0m"
+read a
+echo -e "\033[37;40;0m"
+clear
+echo -e "\033[30;47m  If you love this color press 30\n"
+echo -e "\033[31;40m  If you love this color press 31\n"
+echo -e "\033[32;40m  If you love this color press 32\n"
+echo -e "\033[33;40m  If you love this color press 33\n "
+echo -e "\033[34;40m  If you love this color press 34\n"
+echo -e "\033[35;40m  If you love this color press 35\n"
+echo -e "\033[36;40m  If you love this color press 36\n"
+echo -e "\033[37;40m  If you love this color press 37\n"
+read b
+echo -e "\033[37;40;0m"
+clear
+echo -e "\033[37;40m  To choose these circumstances background enter 40\n "
+echo -e "\033[30;41m  To choose these circumstances background enter 41\n"
+echo -e "\033[30;42m  To choose these circumstances background enter 42\n"
+echo -e "\033[30;43m  To choose these circumstances background enter 43\n"
+echo -e "\033[30;44m  To choose these circumstances background enter 44\n"
+echo -e "\033[30;45m  To choose these circumstances background enter 45\n"
+echo -e "\033[30;46m  To choose these circumstances background enter 46\n"
+echo -e "\033[30;47m  To choose these circumstances background enter 47\n"
+read c
+echo -e "\033[37;40;0m   Enter the text you want to write it"
+read e
+clear
+echo -e "\033[$a;$b;$c;m $e"
+echo -e "\033[37;40;0m"
